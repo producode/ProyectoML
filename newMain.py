58b11a5b1742ec0be9f8f1f0dac631d36fb98956
lgonzalez <produgraf2000@gmail.com>
2023-04-14 10:05:25 -0300

    agregados newMain

--- newMain.py
+++ newMain.py
@@ -29,27 +29,30 @@ data = data.loc[~(data==0).any(axis=1)]
 print(data)
 
 # Dividir los datos en conjuntos de entrenamiento y prueba
-train_data = data.iloc[:400, 2:].astype(float).replace(0, 0.0001)
-test_data = data.iloc[400:, 2:].astype(float).replace(0, 0.0001)
+train_data = data.iloc[:400, 2:].astype(float)
+test_data = data.iloc[400:, 2:].astype(float)
 
 # Separar características de entrada y variable de destino
-X_train = train_data.iloc[:, 1:].astype(float).replace(0, 0.0001)
-y_train = train_data.iloc[:, 0].astype(float).replace(0, 0.0001)
-X_test = test_data.iloc[:, 1:].astype(float).replace(0, 0.0001)
-y_test = test_data.iloc[:, 0].astype(float).replace(0, 0.0001)
+X_train = train_data.iloc[:, 1:].astype(float)
+y_train = train_data.iloc[:, 0].astype(float)
+X_test = test_data.iloc[:, 1:].astype(float)
+y_test = test_data.iloc[:, 0].astype(float)
+
+print(X_train)
+print(X_test)
 
 # Construir modelo de red neuronal
 
 model = Sequential()
-model.add(Dense(2, input_dim=9, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(18, input_shape=(9,), activation='linear'))
+model.add(Dense(9, activation='linear'))
 model.add(Dense(1, activation='sigmoid'))
 
 # Compilar modelo
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='Adadelta', metrics=['accuracy'])
 
 # Ajustar modelo
-model.fit(X_train, y_train, epochs=50, batch_size=10)
+model.fit(X_train, y_train, epochs=200, batch_size=10)
 
 
 # Evaluar precisión del modelo en datos de prueba
@@ -57,7 +60,7 @@ _, accuracy = model.evaluate(X_test, y_test)
 print(y_test)
 print('Precisión: %.2f' % (accuracy*100))
 # Hace una predicción con los últimos datos de la prueba
-last_X_test = X_test.iloc[-5, :]
+last_X_test = X_test.iloc[-1, :]
 last_X_test = np.array(last_X_test).reshape(1, -1)
 prediction = model.predict(last_X_test)
 
